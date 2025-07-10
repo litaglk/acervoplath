de6b528d061b73519adddf7f89b82c369b09079f
Talita Guglak <talitaglk.trad@gmail.com>
2025-07-10 13:14:44 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,50 @@
+let dados = [
+    {
+        titulo: "A Redoma de Vidro",
+        descricao: "A Redoma de Vidro é um romance quase autobiográfico que nos convida a uma profunda viagem pela mente de uma jovem mulher em crescimento",
+        link: "https://pt.wikipedia.org/wiki/Sylvia_Plath",
+        tags: "livro biografia romance"
+      },
+      {
+        titulo: "Ariel",
+        descricao: "Ariel, o livro de poesia da autora, marcado por sua intensidade emocional e exploração de temas como a morte, a loucura e a maternidade",
+        link: "https://pt.wikipedia.org/wiki/Ariel_(livro)",
+        tags: "livro poema"
+      },
+      {
+        titulo: "Os Diários de Sylvia Plath",
+        descricao: "Os Diários de Sylvia Plath traz um olhar íntimo e revelador sobre a vida da autora, desde sua infância até seus últimos anos de vida",
+        link: "https://pt.wikipedia.org/wiki/Os_Diários_de_Sylvia_Plath",
+        tags: "livro diários"
+      },
+      {
+        titulo: "Johnny Panic e a Bíblia dos Sonhos",
+        descricao: "Johnny Panic e a Bíblia dos Sonhos, obra póstuma, é uma coletânea de escritos que traz um labirinto de emoções, medos e desejos",
+        link: "https://en.wikipedia.org/wiki/Johnny_Panic_and_the_Bible_of_Dreams",
+        tags: "livro sonhos contos"
+    },
+    {
+      titulo: "Análise das Múltiplas Identidades de Sylvia Plath Post Mortem",
+      descricao: "Estudo feito por KK, orientado por JCR, sobre como a obra e a pessoa de Sylvia Plath passou a ser retratada após sua morte, em 1963",
+      link: "https://www.puc-rio.br/ensinopesq/ccpg/pibic/relatorio_resumo2019/download/relatorios/CCS/COM/COM-Karen%20Krieger.pdf",
+      tags: "estudo nacional análise"
+    },
+    {
+      titulo: "Sylvia Plath, entre a escrita e o sangue: O trágico como potência do inefável da vida",
+      descricao: "Estudo feito por BVLS, orientado por MCAA, sobre a abordagem do tema morte nas obras da autora",
+      link: "https://repositorio.ufmg.br/bitstream/1843/LETR-AP5GJ4/1/disserta__o___beatriz_viana_lopes_saltarelli.pdf",
+      tags: " estudo nacional niilismo"
+    },
+    {
+      titulo: "A vida após a morte de Sylvia Plath: mutilação de uma obra, censura de uma vida",
+      descricao: "Estudo feito por IGB sobre como a obra e a vida da autora foi conduzida e recortada após sua morte",
+      link: "https://periodicos.ufsc.br/index.php/ref/article/view/86619/54788",
+      tags: "estudo nacional póstumo"
+    },
+    {
+      titulo: "Ecos do Feminino: A Escrita de Sylvia Plath",
+      descricao: "Estudo feito por LMVA, orientado por CPM, sobre o ser mulher pela prisma da psicanálise, usando para essa disucussão obras da autora",
+      link: "https://repositorio.ufpb.br/jspui/bitstream/123456789/30399/1/TCC%20-%20FINALIZADO.pdf",
+      tags: "estudo psicanálise feminismo"
+    }
+    ];
